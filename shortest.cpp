923fef556bacc4a6b1ec48a1a863ed452ba9ec43
root <root@DESKTOP-8969P1M.localdomain>
2026-03-20 16:20:45 -0700

    added comments and header to algo 1

--- shortest.cpp
+++ shortest.cpp
@@ -1,9 +1,16 @@
+/* Names: Ivy Loi, Homan Qiu, Richie Nguyen, Robert Gutierrez
+Emails: iloi05@csu.fullerton.edu, hqiu2006@csu.fullerton.edu, richienguyen@csu.fullerton.edu, lil.rjg3@csu.fullerton.edu
+CPSC 335 section 11
+Project 2: Algorithm 1
+Date: 3/20/26
+*/
 #include <iostream>
 #include <queue>
 #include <vector>
 #include <tuple>
 
 int shortest_path(std::vector<std::vector<int>> grid, std::pair<int, int> start, std::pair<int, int> target) {
+
     int rows = grid.size();
     int cols = grid[0].size();
 
@@ -12,30 +19,41 @@ int shortest_path(std::vector<std::vector<int>> grid, std::pair<int, int> start,
     int tx = target.first;
     int ty = target.second;
 
+    // avoid blocked paths
     if (grid[sx][sy] == 1 || grid[tx][ty] == 1) {
         return -1;
     }
 
+   
+    // pushing start node into the queue
     std::queue<std::tuple<int, int, int>> q;
     q.push({sx, sy, 0});
+    // marking the start node as visited
     std::vector<std::vector<bool>> visited(rows, std::vector<bool>(cols, false));
     visited[sx][sy] = true;
 
+    // establishing the movement that is allowed
     std::vector<std::pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    // looping through vector
     while (!q.empty()) {
         auto [x, y, dist] = q.front();
         q.pop();
+        // once target coordinates == reached, return the distance traveled
         if (x == tx && y == ty) {
             return dist;
         }
+        // looping through vector and moving around
         for (auto [dx, dy] : directions) {
             int nx = x + dx;
             int ny = y + dy;
+            // if the node we're on is in bounds and not marked as visited
             if ((0 <= nx && nx < rows) &&
                 (0 <= ny && ny < cols) &&
                 (grid[nx][ny] == 0) &&
                 !visited[nx][ny]) {
+                // mark it as visited
                 visited[nx][ny] = true;
+                // push it into the queue
                 q.push({nx, ny, dist + 1});
             }
         }     
